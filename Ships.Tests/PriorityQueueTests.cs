f7446b4c0b1a29158c3e86b32902ce34de8ebb25
Miska Rihu <229709985+misrihtuni@users.noreply.github.com>
2026-09-22 18:22:23 +0300

    Tests/Add: Enqueue increases Count in PriorityQueue

--- Ships.Tests/PriorityQueueTests.cs
+++ Ships.Tests/PriorityQueueTests.cs
@@ -3,6 +3,49 @@ using Xunit;
 
 public class PriorityQueueTests
 {
+	#region Enqueue
+
+	[Fact]
+	public void Enqueue_WhenNotAddingDuplicates_IncreasesCount()
+	{
+		PriorityQueue<int> queue = new PriorityQueue<int>();
+
+		queue.Enqueue(1);
+		int count1 = queue.Count;
+
+		queue.Enqueue(0);
+		int count2 = queue.Count;
+
+		queue.Enqueue(-1);
+		int count3 = queue.Count;
+
+		Assert.Equal(1, count1);
+		Assert.Equal(2, count2);
+		Assert.Equal(3, count3);
+	}
+
+	[Fact]
+	public void Enqueue_WhenAddingDuplicates_IncreasesCount()
+	{
+		PriorityQueue<int> queue = new PriorityQueue<int>();
+
+		queue.Enqueue(1);
+		int count1 = queue.Count;
+
+		queue.Enqueue(1);
+		int count2 = queue.Count;
+
+		queue.Enqueue(1);
+		int count3 = queue.Count;
+
+		Assert.Equal(1, count1);
+		Assert.Equal(2, count2);
+		Assert.Equal(3, count3);
+	}
+
+	#endregion Enqueue
+
+
 	#region Peek
 
 	[Fact]
